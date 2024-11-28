0f9513fd3c215f0323ae678b9338b10a11e01e3a
Jonathan Zuniga <jozunigabu@est.utn.ac.cr>
2024-11-27 19:11:05 -0600

    Initial commit for tarea7-Jonathan

---
+++ app/src/main/java/cr/ac/utn/tarea7/DogGalleryActivity.kt
@@ -0,0 +1,51 @@
+package cr.ac.utn.tarea7
+
+import DogImageResponse
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class DogGalleryActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: DogGalleryAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_gallery)
+
+        recyclerView = findViewById(R.id.recyclerViewGallery)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+
+        val breed = intent.getStringExtra("breed") ?: return
+
+        fetchDogImages(breed)
+    }
+
+    private fun fetchDogImages(breed: String) {
+        val apiService = RetrofitClient.instance
+        apiService.getDogImages(breed).enqueue(object : Callback<DogImageResponse> {
+            override fun onResponse(
+                call: Call<DogImageResponse>,
+                response: Response<DogImageResponse>
+            ) {
+                if (response.isSuccessful) {
+                    val images = response.body()?.message ?: emptyList()
+                    adapter = DogGalleryAdapter(images)
+                    recyclerView.adapter = adapter
+                } else {
+                    Log.e("DogGalleryActivity", "Error: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<DogImageResponse>, t: Throwable) {
+                Log.e("DogGalleryActivity", "Failure: ${t.message}")
+            }
+        })
+    }
+}
